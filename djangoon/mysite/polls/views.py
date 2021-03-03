2145b7bf1a4a8e279b03852c1046cbb35099acd5
MGebski <poelki7@gmail.com>
2021-03-03 23:52:01 +0100

    Zgłaszanie błędu 404

--- djangoon/mysite/polls/views.py
+++ djangoon/mysite/polls/views.py
@@ -4,8 +4,10 @@ from django.template import loader
 
 from django.shortcuts import render
 
-from .models import Question
+from django.http import Http404
+from django.shortcuts import render
 
+from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -13,7 +15,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
