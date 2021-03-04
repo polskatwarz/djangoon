6a46fb5f2c02cabde6d3ca73d8a8e6640a3d8751
MGebski <poelki7@gmail.com>
2021-03-04 22:47:39 +0100

    Dostosowanie formularza, powiązanych obiektów i widoków

--- djangoon/mysite/polls/models.py
+++ djangoon/mysite/polls/models.py
@@ -8,9 +8,14 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -18,3 +23,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+    
